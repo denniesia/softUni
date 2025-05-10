b0e8bca6508933952786bd5dcf054f22783c4848
dennie <denitsa.sureva@icloud.com>
2025-05-10 15:25:32 +0200

    feat: album model

--- exprep_musicApp/albums/models.py
+++ exprep_musicApp/albums/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
+from .choices import GenreChoices
 # Create your models here.
+class Album(models.Model):
+    name = models.CharField(
+        max_length=30,
+        unique=True,
+    )
+    artist = models.CharField(
+        max_length=30,
+    )
+    genre = models.CharField(
+        max_length=30,
+        choices=GenreChoices,
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+    )
+    image_url = models.URLField()
+    price = models.FloatField(
+        validators=[
+            MinValueValidator(0.0)
+        ]
+    )
+    owner = models.ForeignKey(
+        'profiles.Profile', #single responsibility, not 'Profile'
+        on_delete=models.CASCADE,
+        related_name='albums',
+    )
