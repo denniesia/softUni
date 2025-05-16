467fbbbe24b98201d4d960232904bfd20778a918
dennie <denitsa.sureva@icloud.com>
2025-05-16 14:52:01 +0200

    fix: post model

--- exprep_furryFunnies/posts/models.py
+++ exprep_furryFunnies/posts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(
+        max_length=50,
+        validators=[
+            MinLengthValidator(5),
+        ],
+        unique=True,
+    )
+    image_url = models.URLField()
+    content = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        to='authors.Author',
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
