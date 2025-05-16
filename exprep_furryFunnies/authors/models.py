6f73a71e3e444e3c0a73170a33c5c54c29096d93
dennie <denitsa.sureva@icloud.com>
2025-05-16 14:40:24 +0200

    Initial commit, author mode

---
+++ exprep_furryFunnies/authors/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.core.validators import MinLengthValidator
+from .validators import OnlyLettersValidator, MaxDigitsValidator
+
+# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(
+        max_length=40,
+        validators=[
+            MinLengthValidator(4),
+            OnlyLettersValidator()
+        ]
+    )
+    last_name = models.CharField(
+        max_length=50,
+        validators=[
+            MinLengthValidator(2),
+            OnlyLettersValidator()
+        ]
+    )
+    passcode = models.CharField(
+        max_length=6,
+        validators=[
+            MinLengthValidator(6),
+            MaxDigitsValidator
+        ]
+    )
+    pets_numbers = models.SmallPositiveIntegerField()
+    info = models.TextField(
+        blank=True,
+        null=True,
+    )
+    image_url = models.URLField(
+        blank=True,
+        null=True,
+    )
---
+++ exprep_furryFunnies/authors/validators.py
@@ -0,0 +1,43 @@
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+
+
+@deconstructible
+class OnlyLettersValidator:
+    def __init__(self, message):
+        self.message = message
+
+    @property
+    def message(self):
+        return self.__message
+
+    @message.setter
+    def message(self, value):
+        if value is not None:
+            self.__message = value
+        else:
+            self.__message = "Your name must contain letters only!"
+
+    def __call__(self, value):
+        if not value.isalpha():
+            raise ValidationError(self.message)
+
+@deconstructible
+class MaxDigitsValidator:
+    def __init__(self, message):
+        self.message = message
+
+    @property
+    def message(self):
+        return self.__message
+
+    @message.setter
+    def message(self, value):
+        if value is not None:
+            self.__message = value
+        else:
+            self.__message = "Your passcode must be exactly 6 digits!"
+
+    def __call__(self, value):
+        if len(value) != 6:
+            raise ValidationError(self.message)
