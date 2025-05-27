f0862e3e9595a7792a2551d1a15630d75b681741
dennie <denitsa.sureva@icloud.com>
2025-05-27 20:02:25 +0200

    feat: profile details page

--- exprep_WorldOfSpeed/profiles/urls.py
+++ exprep_WorldOfSpeed/profiles/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create',views.CreateProfileView.as_view(), name='profile-create' ),
+    path('details',views.ProfileDetailsView.as_view(), name='profile-details' ),
 ]
--- exprep_WorldOfSpeed/profiles/views.py
+++ exprep_WorldOfSpeed/profiles/views.py
@@ -2,8 +2,23 @@ from django.shortcuts import render
 from django.views.generic import  CreateView, DetailView, UpdateView, DeleteView
 from .forms import ProfileCreateForm
 from django.urls import reverse_lazy
+from exprep_WorldOfSpeed.utils import get_user_obj
+from .models import Profile
 # Create your views here.
 class CreateProfileView(CreateView):
     template_name = 'profiles/profile-create.html'
     form_class = ProfileCreateForm
     success_url = reverse_lazy('catalogue')
+
+class ProfileDetailsView(DetailView):
+    template_name = 'profiles/profile-details.html'
+
+    def get_object(self, queryset=None):
+        return get_user_obj()
+
+    def get_context_data(self,*args, **kwargs):
+        context = super().get_context_data(*args,**kwargs)
+        total_all_cars = self.object.cars.all()
+        total_sum = sum(car.price for car in total_all_cars )
+        context['total_sum'] = total_sum
+        return context
