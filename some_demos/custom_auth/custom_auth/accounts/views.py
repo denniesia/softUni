29c811a8f2dd547fc0dc1d3d71eaf7eab87f9da9
dennie <denitsa.sureva@icloud.com>
2025-06-03 18:32:49 +0200

    user extending demo

---
+++ some_demos/custom_auth/custom_auth/accounts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.contrib.auth import views as auth_views
+# Create your views here.
+class LoginUserView(auth_views.LoginView):
+    template_name = 'accounts/login.html'
+
+
+
+class RegisterUserView():
+    pass
