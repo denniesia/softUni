65d7255f7d4a8f7814061329edb726725d9f933b
dennie <denitsa.sureva@icloud.com>
2025-06-16 16:15:34 +0200

    feat: recipe model

--- exprep_tastyRecipes/exprep_tastyRecipes/recipe/models.py
+++ exprep_tastyRecipes/exprep_tastyRecipes/recipe/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MinValueValidator
+from .choices import CuisineTypes
 
 # Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(
+        max_length=100,
+        unique=True,
+        validators=[
+            MinLengthValidator(10),
+        ]
+    )
+    cuisine_type = models.CharField(
+        max_length=7,
+        choices=CuisineTypes,
+    )
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    cooking_time = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(1),
+        ]
+    )
+    image_url = models.URLField(
+        blank=True,
+        null=True,
+    )
+    author = models.ForeignKey(
+        'profiles.Profile',
+        on_delete=models.CASCADE,
+    )
