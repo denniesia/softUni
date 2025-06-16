0280a4022fd239cde70d4c8b0a304e74da07f554
dennie <denitsa.sureva@icloud.com>
2025-06-16 18:01:07 +0200

    feat: home page

--- exprep_tastyRecipes/exprep_tastyRecipes/common/templatetags/get_user.py
+++ exprep_tastyRecipes/exprep_tastyRecipes/common/templatetags/get_user.py
@@ -4,6 +4,5 @@ from exprep_tastyRecipes.utils import get_user_obj
 register = template.Library()
 
 @register.simple_tag(name='get_user')
-def get_user(user):
-    print(get_user_obj())
+def get_user():
     return get_user_obj()
