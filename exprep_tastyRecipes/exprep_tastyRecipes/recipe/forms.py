f633d39bade5a329b189a5bfa89533b0c7c01ec1
dennie <denitsa.sureva@icloud.com>
2025-06-17 16:09:15 +0200

    fix: create recipe help texts

--- exprep_tastyRecipes/exprep_tastyRecipes/recipe/forms.py
+++ exprep_tastyRecipes/exprep_tastyRecipes/recipe/forms.py
@@ -8,6 +8,12 @@ class RecipeBaseForm(forms.ModelForm):
 
 
 class RecipeCreateForm(RecipeBaseForm):
-    title = forms.CharField(
-        error_messages={'unique': "We already have a recipe with the same title!" },
-    )
+    class Meta(RecipeBaseForm.Meta):
+        title = forms.CharField(
+            error_messages={'unique': "We already have a recipe with the same title!" },
+        )
+
+        help_texts = {
+            'ingredients': 'Ingredients must be separated by a comma and space!',
+            'cooking_time': 'Provide the cooking time in minutes',
+        }
